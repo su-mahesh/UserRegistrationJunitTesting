fcb2c19cc7f4f2ff74e98a3153bd04df7c731377
su-mahesh <maheshkangude@gmail.com>
2021-01-18 20:09:57 +0530

    REFACTORED

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     final private String firstNamePattern = "[A-Z][a-z]{2,}";
     final private String lastNamePattern = "[A-Z][a-z]{2,}";
-    final private String emailAddressPattern = "[A-Za-z0-9_][+A-Za-z0-9_-]*[.]?[+A-Za-z0-9_-]+[@]([A-Za-z0-9]{1,}[.][A-Za-z]{2,63}|[A-Za-z0-9-]{2,}[.][A-Za-z0-9]{2,63}[.][A-Za-z]{2,3})";
+    final private String emailAddressPattern = "[A-Za-z0-9_][+A-Za-z0-9_-]*[.]?[+A-Za-z0-9_-]+[@]([A-Za-z0-9]+[.][A-Za-z]{2,63}|[A-Za-z0-9-]{2,}[.][A-Za-z0-9]{2,63}[.][A-Za-z]{2,3})";
     final private String mobileNumberPattern = "^[1-9][0-9]{0,2}\\s[0-9]{10}";
     final private String passwordPattern = "^(?=.{8,20}$)(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9]*[^A-Za-z0-9][A-Za-z0-9]*$";
 
